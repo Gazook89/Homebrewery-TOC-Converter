04faf5ba137e29ecda1cec645be47af9a9a9e7f8
Gazook89 <58999374+Gazook89@users.noreply.github.com>
2021-10-14 21:21:34 -0400

    add regex to handle different levels of headings
    uses some pretty ugly repeating regex matches.  could likely be refactored.
    
    fixes #1

--- scripts.js
+++ scripts.js
@@ -25,12 +25,16 @@ function convertLegacytoFancyLegacy() {
     // const regexFirstLine = /^(\s*<div\s)(.*)(>)$/gm;
     const regexTOC = /.*class=['"].*?toc.*?['"].*/gm;
     const regexTitle = /^##### (.*)/gm;
-    const regexHeading = /^( *).*\d (.*)\].*(\d+).*/gm;
+    const regexHeading1 = /^-.*\d (.*)\].*(\d+).*/gm;
+    const regexHeading2 = /^(?: {2}|\t)-.*\d (.*)\].*(\d+).*/gm;
+    const regexHeading3 = /^(?: {4}|\t{2})-.*\d (.*)\].*(\d+).*/gm;
     const regexClose = /<\/div>/gm;
     if(legacyText.match(regexTOC)){
         v3Text = legacyText.replace(regexTOC, `<div class="toc wide">\n`);
         v3Text = v3Text.replace(regexTitle, `# $1\n`);
-        v3Text = v3Text.replace(regexHeading, `$1- ### [<span>$2</span><span>$3</span>](#p$3)`);
+        v3Text = v3Text.replace(regexHeading1, `- ### [<span>$1</span><span>$2</span>](#p$2)`);
+        v3Text = v3Text.replace(regexHeading2, `  - #### [<span>$1</span><span>$2</span>](#p$2)`);
+        v3Text = v3Text.replace(regexHeading3, `    - [<span>$1</span><span>$2</span>](#p$2)`);
         v3Text = v3Text.replace(regexClose, `\n</div>`);
         document.getElementById("outputText").value = v3Text;
         document.getElementById("outputStyle").value = provideStyle('legacy');
@@ -42,95 +46,95 @@ function convertLegacytoFancyLegacy() {
 function provideStyle() {
     return `/* ToC Styling */\n
 .page .toc {
--webkit-column-break-inside:avoid;
-page-break-inside:avoid;
-break-inside:avoid
+    -webkit-column-break-inside:avoid;
+    page-break-inside:avoid;
+    break-inside:avoid
 }
 
 .page .toc h1 {
-text-align:center;
-margin-bottom:0cm
+    text-align:center;
+    margin-bottom:0cm
 }
 
 .page .toc a {
-display:table;
-color:inherit;
-text-decoration:none
+    display:table;
+    color:inherit;
+    text-decoration:none
 }
 
 .page .toc a:hover {
-text-decoration:underline
+    text-decoration:underline
 }
 
 .page .toc h4 {
-margin-top:.14cm
+    margin-top:.14cm
 }
 
 .page .toc h4+ul li {
-line-height:1.2em
+    line-height:1.2em
 }
 
 .page .toc>ul {
-margin-top:.52cm
+    margin-top:.52cm
 }
 
 .page .toc ul {
-padding-left:0;
-list-style-type:none
+    padding-left:0;
+    list-style-type:none
 }
 
 .page .toc ul li+li h3 {
-margin-top:.26cm;
-line-height:1em
+    margin-top:.26cm;
+    line-height:1em
 }
 
 .page .toc ul h3 span:first-child::after {
-border:none
+    border:none
 }
 
 .page .toc ul span {
-display:table-cell
+    display:table-cell
 }
 
 .page .toc ul span:first-child {
-position:relative;
-overflow:hidden
+    position:relative;
+    overflow:hidden
 }
 
 .page .toc ul span:first-child::after {
-content:"";
-position:absolute;
-bottom:.08cm;
-margin-left:.06cm;
-width:100%;
-border-bottom:.05cm dotted #000
+    content:"";
+    position:absolute;
+    bottom:.08cm;
+    margin-left:.06cm;
+    width:100%;
+    border-bottom:.05cm dotted #000
 }
 
 .page .toc ul span:last-child {
-font-family:BookInsanityRemake;
-font-size:.34cm;
-font-weight:normal;
-color:black;
-text-align:right;
-vertical-align:bottom;
-width:1%;
-padding-left:.06cm
+    font-family:BookInsanityRemake;
+    font-size:.34cm;
+    font-weight:normal;
+    color:black;
+    text-align:right;
+    vertical-align:bottom;
+    width:1%;
+    padding-left:.06cm
 }
 
 .page .toc ul ul {
-margin-left:1em
+    margin-left:1em
 }
 
 .page .toc.wide {
-column-count:2;
-column-fill:balance;
-column-gap:.9cm;
-column-width:7.68cm;
--webkit-column-count:2;
--moz-column-count:2;
--webkit-column-width:7.68cm;
--moz-column-width:7.68cm;
--webkit-column-gap:.9cm;
--moz-column-gap:.9cm
+    column-count:2;
+    column-fill:balance;
+    column-gap:.9cm;
+    column-width:7.68cm;
+    -webkit-column-count:2;
+    -moz-column-count:2;
+    -webkit-column-width:7.68cm;
+    -moz-column-width:7.68cm;
+    -webkit-column-gap:.9cm;
+    -moz-column-gap:.9cm
 }`
 }
